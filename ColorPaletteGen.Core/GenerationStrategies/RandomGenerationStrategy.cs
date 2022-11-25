28463084cf1c9b6ca611e9075e1570eb1819cecb
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-11-25 20:08:43 +0200

    added generation strategy interface and implementation

---
+++ ColorPaletteGen.Core/GenerationStrategies/RandomGenerationStrategy.cs
@@ -0,0 +1,18 @@
+﻿namespace ColorPaletteGen.Core.GenerationStrategies;
+
+public class RandomGenerationStrategy : IGenerationStrategy
+{
+    private static readonly Random Rand = new();
+    public List<Color> Generate(int count)
+    {
+        List <Color> colors = new(count);
+        var bytes = new byte[3];
+        for (var i = 0; i < 5; i++)
+        {
+            Rand.NextBytes(bytes);
+            colors.Add(Color.FromRGB(bytes));
+        }
+
+        return colors;
+    }
+}

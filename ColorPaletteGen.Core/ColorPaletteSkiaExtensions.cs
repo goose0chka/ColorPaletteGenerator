4fa7a1d8d8c3319348270cd81e43f6630f9dc7b5
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-11-26 22:28:45 +0200

    improved skia extensions

--- ColorPaletteGen.Core/ColorPaletteSkiaExtensions.cs
+++ ColorPaletteGen.Core/ColorPaletteSkiaExtensions.cs
@@ -6,10 +6,42 @@ public static class ColorPaletteSkiaExtensions
 {
     public static async Task GenerateImageFileAsync(this ColorPalette palette, int width, int height, string path)
     {
+        using var data = GetData(palette, width, height);
+        path = GetPath(path);
+
+        await using var stream = File.OpenWrite(path);
+        data.SaveTo(stream);
+    }
+
+    private static string GetPath(string path)
+    {
+        var directory = Path.GetDirectoryName(path)
+            ?? throw new ArgumentException("Directory cannot be null");
+        var file = Path.GetFileName(path);
+        
+        if (Path.GetExtension(file) != "png")
+        {
+            throw new ArgumentException("File must have png extension");
+        }
+
+        Directory.CreateDirectory(directory);
+        if (!Path.HasExtension(file))
+        {
+            file = Path.ChangeExtension(path, "png");
+        }
+
+        return Path.Combine(directory, file);
+    }
+
+    public static Stream GetImageStream(this ColorPalette palette, int width, int height) 
+        => GetData(palette, width, height).AsStream();
+
+    private static SKData GetData(ColorPalette palette, int width, int height)
+    {
         using SKBitmap bitmap = new(width, height);
         using SKCanvas canvas = new(bitmap);
 
-        var widthPerColor = width / palette.ColorCount; 
+        var widthPerColor = width / palette.ColorCount;
         for (var i = 0; i < palette.ColorCount; i++)
         {
             var x = widthPerColor * i;
@@ -21,17 +53,9 @@ public static class ColorPaletteSkiaExtensions
             canvas.DrawRect(x, 0, widthPerColor, height, pen);
         }
 
-        using var data = bitmap.Encode(SKEncodedImageFormat.Png, 80);
-
-        if (Path.GetExtension(path) != "png")
-        {
-            path = Path.ChangeExtension(path, "png");
-        }
-
-        await using var stream = File.OpenWrite(path);
-        data.SaveTo(stream);
+        return bitmap.Encode(SKEncodedImageFormat.Png, 20);
     }
-    
+
     private static SKColor ToSkiaColor(this Color c)
-        => new SKColor(c.Red, c.Green, c.Blue);
+        => new (c.Red, c.Green, c.Blue);
 }

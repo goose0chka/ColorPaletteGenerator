de865506d5e36f122277aefc3deed0022cd65448
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-11-27 20:19:43 +0200

    implemented analogous generation strategy

---
+++ ColorPaletteGen.Core/GenerationStrategies/AnalogousGenerationStrategy.cs
@@ -0,0 +1,35 @@
+﻿using ColorPaletteGen.Core.Extensions;
+
+namespace ColorPaletteGen.Core.GenerationStrategies;
+
+public class AnalogousGenerationStrategy : IGenerationStrategy
+{
+    private readonly int _distance;
+    private readonly Color _baseColor;
+
+    public AnalogousGenerationStrategy(Color baseColor, int distance = 15)
+    {
+        _baseColor = baseColor;
+        _distance = distance;
+    }
+
+    public void Generate(Color[] colors)
+    {
+        // throw new NotImplementedException();
+        if (colors.Length % 2 != 1)
+        {
+            throw new InvalidOperationException("Analogous palette should have uneven color count");
+        }
+        
+        var sideCount = colors.Length / 2;
+        var hue = _baseColor.GetHue();
+        var sat = _baseColor.GetSaturation();
+        var val = _baseColor.GetValue();
+        for (var i = 0; i < colors.Length; i++)
+        {
+            var newHue = hue + _distance * (i - sideCount);
+            var color = ColorHSVExtensions.FromHSV(newHue, sat, val);
+            colors[i] = color;
+        }
+    }
+}

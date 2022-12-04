b6a805c664e06bf2a2556a17a460083d4d31e6fd
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-12-05 00:12:55 +0200

    added extension classes

---
+++ ColorPaletteGen.Core/Extensions/ColorPaletteExtensions.cs
@@ -0,0 +1,16 @@
+﻿using ColorPaletteGen.DAL.Model;
+
+namespace ColorPaletteGen.Core.Extensions;
+
+public static class ColorPaletteExtensions
+{
+    public static void InvertLock(this ColorPalette palette, int index)
+    {
+        if (index < 0 || index > palette.Count)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range");
+        }
+        var color = palette.Colors[index];
+        color.Locked = !color.Locked;
+    }
+}

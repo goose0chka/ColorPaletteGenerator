3628cba2dffddaa592ab62916af656acc9b54845
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-12-09 15:30:45 +0200

    added webhook config

---
+++ ColorPaletteGen.Bot/BotConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace ColorPaletteGen.Bot;
+
+public class BotConfiguration
+{
+    public const string ConfigurationSection = "BotConfig";
+    public string Token { get; set; }
+    public string Host { get; set; }
+    public string Endpoint { get; set; }
+}
---
+++ ColorPaletteGen.Bot/Services/WebhookConfigurationService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Options;
+using Telegram.Bot;
+
+namespace ColorPaletteGen.Bot.Services;
+
+public class WebhookConfigurationService : IHostedService
+{
+    private readonly ITelegramBotClient _bot;
+    private readonly ILogger<WebhookConfigurationService> _logger;
+    private readonly BotConfiguration _config;
+
+    public WebhookConfigurationService(ITelegramBotClient bot, 
+        ILogger<WebhookConfigurationService> logger, 
+        IOptions<BotConfiguration> config)
+    {
+        _bot = bot;
+        _logger = logger;
+        _config = config.Value;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        
+        var url = $"{_config.Host}{_config.Endpoint}";
+        _logger.LogInformation("Setting webhook: {Url}", url);
+        await _bot.SetWebhookAsync(url, cancellationToken: cancellationToken);
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Removing webhook");
+        await _bot.DeleteWebhookAsync(cancellationToken: cancellationToken);
+    }
+}
